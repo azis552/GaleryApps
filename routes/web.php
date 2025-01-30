b5a9e578af957035f219e5aea7aba67d47bf831c
azis552 <azisaan552@gmail.com>
2025-01-31 06:29:28 +0700

    login and register

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('beranda');
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('users/logout', [UserController::class, 'logout'])->name('users.logout');
+});
+
+Route::resource('users', UserController::class);
+Route::POST('users/login',[UserController::class,'login'])->name('users.login');
+Route::resource('beranda', BerandaController::class);
